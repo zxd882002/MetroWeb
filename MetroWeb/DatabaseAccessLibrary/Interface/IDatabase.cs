39db7fc72a743dd5256f4ce97c7bc1d848f88735
zxd882002 <zxd882002@126.com>
2016-06-28 23:04:14 +0800

    just a test for git extension tool

--- MetroWeb/DatabaseAccessLibrary/Interface/IDatabase.cs
+++ MetroWeb/DatabaseAccessLibrary/Interface/IDatabase.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace DatabaseAccessLibrary.Interface
 {
     interface IDatabase
     {
+        string ConnectionString { get; set; }
     }
 }
