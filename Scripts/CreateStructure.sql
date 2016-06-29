7d84a51f40000750b3dbf8bd5dd2ab1a119cec0b
zxd882002 <zxd882002@126.com>
2016-06-29 18:00:06 +0800

    enter data for line1, line2

--- Scripts/CreateStructure.sql
+++ Scripts/CreateStructure.sql
@@ -18,7 +18,8 @@ CREATE TABLE Line -- 线路信息
 (
 	Line_Id INT PRIMARY KEY,
 	Line_Name VARCHAR(20),
-	Line_Direction_Station_Id INT FOREIGN KEY REFERENCES Station(Station_Id)
+	Line_From_Station_Id INT FOREIGN KEY REFERENCES Station(Station_Id),
+	Line_To_Station_Id INT FOREIGN KEY REFERENCES Station(Station_Id)
 )
 GO
 
@@ -27,7 +28,6 @@ CREATE TABLE StationLine -- 站台信息
 	Station_Line_Id INT PRIMARY KEY,
 	Line_Id INT FOREIGN KEY REFERENCES Line(Line_Id),
 	Station_Id INT FOREIGN KEY REFERENCES Station(Station_Id),
-	Index_Number INT,
 	Duration TIME,
 	Cost_Arrived TIME,
 	Start_Time TIME,
@@ -54,9 +54,10 @@ CREATE TABLE OuterChange -- 站外换乘
 GO
 
 CREATE VIEW V_StationLine AS
-SELECT l.Line_Name, s2.Station_Name AS 'Direction', s.Station_Name, sl.Index_Number, sl.Duration, sl.Cost_Arrived, sl.Start_Time, sl.End_Time
+SELECT l.Line_Name, s2.Station_Name AS 'From', s3.Station_Name AS 'To', s.Station_Name, sl.Index_Number, sl.Duration, sl.Cost_Arrived, sl.Start_Time, sl.End_Time
 FROM StationLine sl 
 INNER JOIN Line l ON l.Line_Id = sl.Line_Id
 INNER JOIN Station s ON s.Station_Id = sl.Station_Id
-INNER JOIN Station s2 ON l.Line_Direction_Station_Id = s2.Station_Id
+INNER JOIN Station s2 ON l.Line_From_Station_Id = s2.Station_Id
+INNER JOIN Station s3 ON l.Line_To_Station_Id = s3.Station_Id
 GO
