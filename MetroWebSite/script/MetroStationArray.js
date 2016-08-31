75324fa8af6c55716af061ef3a479e61b45c1317
zxd882002 <zxd882002@126.com>
2016-08-31 21:12:03 +0800

    add station and line

--- MetroWebSite/script/MetroStationArray.js
+++ MetroWebSite/script/MetroStationArray.js
@@ -18,6 +18,31 @@
 	{
         id: 104,
         StationGraph: {x: 220, y: 410},
-        NameGraph: {x: 250, y: 415, text: '锦江乐园'}
+        NameGraph: {x: 255, y: 415, text: '锦江乐园'}
+    },
+	{
+        id: 105,
+        StationGraph: {x: 260, y: 380},
+        NameGraph: {x: 295, y: 385, text: '上海南站'}
+    },
+	{
+        id: 106,
+        StationGraph: {x: 260, y: 340},
+        NameGraph: {x: 290, y: 345, text: '漕宝路'}
+    },
+	{
+        id: 107,
+        StationGraph: {x: 300, y: 310},
+        NameGraph: {x: 340, y: 315, text: '上海体育馆'}
+    },
+	{
+        id: 108,
+        StationGraph: {x: 340, y: 280},
+        NameGraph: {x: 370, y: 285, text: '徐家汇'}
+    },
+	{
+        id: 108,
+        StationGraph: {x: 380, y: 250},
+        NameGraph: {x: 410, y: 255, text: '衡山路'}
     }
 ];
