522486bca3acecc273f05f3cf57554735c10d7db
zxd882002 <zxd882002@126.com>
2016-10-11 22:52:46 +0800

    add line 1

--- MetroWebSite/script/MetroStationArray.js
+++ MetroWebSite/script/MetroStationArray.js
@@ -2,47 +2,147 @@
 [
     {
         id: 101,
-        StationGraph: {x: 100, y: 500},
-        NameGraph: {x: 125, y: 505, text: '莘庄'}
+        StationGraph: {x: 180, y: 550},
+        NameGraph: {x: 205, y: 555, text: '莘庄'}
     },
     {
         id: 102,
-        StationGraph: {x: 140, y: 470},
-        NameGraph: {x: 170, y: 475, text: '外环路'}
+        StationGraph: {x: 220, y: 520},
+        NameGraph: {x: 250, y: 525, text: '外环路'}
     },
 	{
         id: 103,
-        StationGraph: {x: 180, y: 440},
-        NameGraph: {x: 210, y: 445, text: '莲花路'}
+        StationGraph: {x: 260, y: 490},
+        NameGraph: {x: 290, y: 495, text: '莲花路'}
     },
 	{
         id: 104,
-        StationGraph: {x: 220, y: 410},
-        NameGraph: {x: 255, y: 415, text: '锦江乐园'}
+        StationGraph: {x: 300, y: 460},
+        NameGraph: {x: 335, y: 465, text: '锦江乐园'}
     },
 	{
         id: 105,
-        StationGraph: {x: 260, y: 380},
-        NameGraph: {x: 295, y: 385, text: '上海南站'}
+        StationGraph: {x: 340, y: 430},
+        NameGraph: {x: 375, y: 435, text: '上海南站'}
     },
 	{
         id: 106,
-        StationGraph: {x: 260, y: 340},
-        NameGraph: {x: 290, y: 345, text: '漕宝路'}
+        StationGraph: {x: 340, y: 390},
+        NameGraph: {x: 370, y: 395, text: '漕宝路'}
     },
 	{
         id: 107,
-        StationGraph: {x: 300, y: 310},
-        NameGraph: {x: 340, y: 315, text: '上海体育馆'}
+        StationGraph: {x: 380, y: 360},
+        NameGraph: {x: 420, y: 365, text: '上海体育馆'}
     },
 	{
         id: 108,
-        StationGraph: {x: 340, y: 280},
-        NameGraph: {x: 370, y: 285, text: '徐家汇'}
+        StationGraph: {x: 420, y: 330},
+        NameGraph: {x: 450, y: 335, text: '徐家汇'}
     },
 	{
-        id: 108,
-        StationGraph: {x: 380, y: 250},
-        NameGraph: {x: 410, y: 255, text: '衡山路'}
+        id: 109,
+        StationGraph: {x: 460, y: 300},
+        NameGraph: {x: 490, y: 305, text: '衡山路'}
+    },
+	{
+        id: 110,
+        StationGraph: {x: 500, y: 270},
+        NameGraph: {x: 520, y: 285, text: '常熟路'}
+    },
+	{
+        id: 111,
+        StationGraph: {x: 550, y: 270},
+        NameGraph: {x: 580, y: 285, text: '陕西南路'}
+    },
+	{
+        id: 112,
+        StationGraph: {x: 640, y: 255},
+        NameGraph: {x: 670, y: 270, text: '黄陂南路'}
+    },
+	{
+        id: 113,
+        StationGraph: {x: 640, y: 220},
+        NameGraph: {x: 670, y: 205, text: '人民广场'}
+    },
+	{
+        id: 114,
+        StationGraph: {x: 615, y: 180},
+        NameGraph: {x: 640, y: 165, text: '新闸路'}
+    },
+	{
+        id: 115,
+        StationGraph: {x: 590, y: 140},
+        NameGraph: {x: 615, y: 125, text: '汉中路'}
+    },
+	{
+        id: 116,
+        StationGraph: {x: 535, y: 105},
+        NameGraph: {x: 570, y: 90, text: '上海火车站'}
+    },
+	{
+        id: 117,
+        StationGraph: {x: 535, y: 70},
+        NameGraph: {x: 565, y: 55, text: '中山北路'}
+    },
+	{
+        id: 118,
+        StationGraph: {x: 535, y: 35},
+        NameGraph: {x: 560, y: 20, text: '延长路'}
+    },
+	{
+        id: 119,
+        StationGraph: {x: 535, y: 0},
+        NameGraph: {x: 570, y: -15, text: '上海马戏城'}
+    },
+	{
+        id: 120,
+        StationGraph: {x: 535, y: -35},
+        NameGraph: {x: 560, y: -50, text: '汶水路'}
+    },
+	{
+        id: 121,
+        StationGraph: {x: 535, y: -70},
+        NameGraph: {x: 565, y: -85, text: '彭浦新村'}
+    },
+	{
+        id: 122,
+        StationGraph: {x: 535, y: -105},
+        NameGraph: {x: 560, y: -120, text: '共康路'}
+    },
+	{
+        id: 123,
+        StationGraph: {x: 535, y: -140},
+        NameGraph: {x: 565, y: -155, text: '通河新村'}
+    },
+	{
+        id: 124,
+        StationGraph: {x: 535, y: -175},
+        NameGraph: {x: 560, y: -190, text: '呼兰路'}
+    },
+	{
+        id: 125,
+        StationGraph: {x: 535, y: -210},
+        NameGraph: {x: 565, y: -225, text: '共富新村'}
+    },
+	{
+        id: 126,
+        StationGraph: {x: 535, y: -245},
+        NameGraph: {x: 565, y: -260, text: '宝安公路'}
+    },
+	{
+        id: 127,
+        StationGraph: {x: 535, y: -280},
+        NameGraph: {x: 565, y: -295, text: '友谊西路'}
+    },
+	{
+        id: 128,
+        StationGraph: {x: 535, y: -315},
+        NameGraph: {x: 560, y: -330, text: '富锦路'}
+    },	
+	{
+        id: 221,
+        StationGraph: {x: 550, y: 220},
+        NameGraph: {x: 580, y: 235, text: '南京西路'}
     }
 ];
--- MetroWebSite/script/StationLineArray.js
+++ MetroWebSite/script/StationLineArray.js
@@ -11,41 +11,33 @@
             strokeWidth: 4,
             p1: {
                 type: 'line',
-                x1: 100,
-                y1: 500,
-                x2: 260,
-                y2: 380
-            },
-			p2: {
-                type: 'line',
-				x1: 260,
-                y1: 380,
-                x2: 260,
-                y2: 340
+                x1: 180, y1: 550,
+                x2: 340, y2: 430,
+				x3: 340, y3: 390,
+				x4: 500, y4: 270,
+				x5: 550, y5: 270,
+				x6: 640, y6: 255,
+				x7: 640, y7: 220,
+				x8: 590, y8: 140,
+				x9: 535, y9: 105,
+				x10: 535, y10: -315
             }
-			/*			
-			p1: {
+        }
+    },
+	{
+        LineGraph: {
+            layer: true,
+            draggable: true,
+            groups: ['Metros'],
+            dragGroups: ['Metros'],
+            type: 'path',
+            strokeStyle: '#009900',
+            strokeWidth: 4,
+            p1: {
                 type: 'line',
-                x1: 100,
-                y1: 500,
-                x2: 140,
-                y2: 470
-            },
-			p2: {
-                type: 'line',
-                x1: 140,
-                y1: 470,
-                x2: 180,
-                y2: 440
-            },
-			p3: {
-                type: 'line',
-                x1: 180,
-                y1: 440,
-                x2: 220,
-                y2: 410
+                x1: 550, y1: 220,
+                x2: 640, y2: 220
             }
-			*/
         }
     }
 ];
