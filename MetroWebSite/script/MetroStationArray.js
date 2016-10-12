41d7d3794a5e50e343700bce26f1e15c1067a4d3
zxd882002 <zxd882002@126.com>
2016-10-12 22:12:24 +0800

    add line 2

--- MetroWebSite/script/MetroStationArray.js
+++ MetroWebSite/script/MetroStationArray.js
@@ -1,5 +1,6 @@
 ﻿var metroStationArray =
 [
+    // #1
     {
         id: 101,
         StationGraph: {x: 180, y: 550},
@@ -139,10 +140,153 @@
         id: 128,
         StationGraph: {x: 535, y: -315},
         NameGraph: {x: 560, y: -330, text: '富锦路'}
-    },	
+    },
+	// #2
 	{
-        id: 221,
+        id: 201,
+        StationGraph: {x: -140, y: 265},
+        NameGraph: {x: -140, y: 250, text: '徐泾东'}
+    },
+	{
+        id: 202,
+        StationGraph: {x: -70, y: 270},
+        NameGraph: {x: -70, y: 255, text: '虹桥火车站'}
+    },
+	{
+        id: 203,
+        StationGraph: {x: 0, y: 270},
+        NameGraph: {x: 0, y: 285, text: '虹桥2号航站楼'}
+    },
+	{
+        id: 204,
+        StationGraph: {x: 70, y: 220},
+        NameGraph: {x: 95, y: 235, text: '淞虹路'}
+    },
+	{
+        id: 205,
+        StationGraph: {x: 140, y: 220},
+        NameGraph: {x: 165, y: 235, text: '北新泾'}
+    },
+	{
+        id: 206,
+        StationGraph: {x: 210, y: 220},
+        NameGraph: {x: 235, y: 235, text: '威宁路'}
+    },
+	{
+        id: 207,
+        StationGraph: {x: 280, y: 220},
+        NameGraph: {x: 310, y: 235, text: '娄山关路'}
+    },
+	{
+        id: 208,
+        StationGraph: {x: 350, y: 220},
+        NameGraph: {x: 380, y: 235, text: '中山公园'}
+    },
+	{
+        id: 209,
+        StationGraph: {x: 420, y: 220},
+        NameGraph: {x: 445, y: 235, text: '江苏路'}
+    },
+	{
+        id: 210,
+        StationGraph: {x: 500, y: 220},
+        NameGraph: {x: 520, y: 235, text: '静安寺'}
+    },
+	{
+        id: 211,
         StationGraph: {x: 550, y: 220},
         NameGraph: {x: 580, y: 235, text: '南京西路'}
+    },
+	{
+        id: 212,
+        StationGraph: {x: 720, y: 220},
+        NameGraph: {x: 750, y: 235, text: '南京东路'}
+    },
+	{
+        id: 213,
+        StationGraph: {x: 790, y: 220},
+        NameGraph: {x: 815, y: 205, text: '陆家嘴'}
+    },
+	{
+        id: 214,
+        StationGraph: {x: 820, y: 250},
+        NameGraph: {x: 845, y: 235, text: '东昌路'}
+    },
+	{
+        id: 215,
+        StationGraph: {x: 850, y: 280},
+        NameGraph: {x: 880, y: 265, text: '世纪大道'}
+    },
+	{
+        id: 216,
+        StationGraph: {x: 880, y: 310},
+        NameGraph: {x: 915, y: 295, text: '上海科技馆'}
+    },
+	{
+        id: 217,
+        StationGraph: {x: 910, y: 340},
+        NameGraph: {x: 940, y: 325, text: '世纪公园'}
+    },
+	{
+        id: 218,
+        StationGraph: {x: 940, y: 370},
+        NameGraph: {x: 910, y: 370, text: '龙阳路'}
+    },
+	{
+        id: 219,
+        StationGraph: {x: 1010, y: 370},
+        NameGraph: {x: 1010, y: 385, text: '张江高科'}
+    },
+	{
+        id: 220,
+        StationGraph: {x: 1080, y: 370},
+        NameGraph: {x: 1080, y: 385, text: '金科路'}
+    },
+	{
+        id: 221,
+        StationGraph: {x: 1150, y: 370},
+        NameGraph: {x: 1150, y: 385, text: '广兰路'}
+    },
+	{
+        id: 222,
+        StationGraph: {x: 1220, y: 370},
+        NameGraph: {x: 1220, y: 385, text: '唐镇'}
+    },
+	{
+        id: 223,
+        StationGraph: {x: 1290, y: 370},
+        NameGraph: {x: 1290, y: 355, text: '创新中路'}
+    },
+	{
+        id: 224,
+        StationGraph: {x: 1290, y: 420},
+        NameGraph: {x: 1250, y: 420, text: '华夏东路'}
+    },
+	{
+        id: 225,
+        StationGraph: {x: 1290, y: 470},
+        NameGraph: {x: 1265, y: 470, text: '川沙'}
+    },
+	{
+        id: 226,
+        StationGraph: {x: 1290, y: 520},
+        NameGraph: {x: 1260, y: 520, text: '凌空路'}
+    },
+	{
+        id: 227,
+        StationGraph: {x: 1290, y: 570},
+        NameGraph: {x: 1250, y: 570, text: '远东大道'}
+    },
+	{
+        id: 228,
+        StationGraph: {x: 1290, y: 620},
+        NameGraph: {x: 1250, y: 620, text: '海天三路'}
+    },
+	{
+        id: 229,
+        StationGraph: {x: 1290, y: 670},
+        NameGraph: {x: 1240, y: 670, text: '浦东国际机场'}
     }
+	// #3
+	// #4
 ];
